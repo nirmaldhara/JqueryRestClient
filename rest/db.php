18fc4dbd3052fd3e3b72441dfc2e30685e7c42f5
Nirmal <nirmal.dhara@gmail.com>
2015-07-16 18:45:21 +0530

    Delete db.php

--- rest/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$mysql_hostname = "localhost";
-$mysql_user = "codewale_bang1";
-$mysql_password = "kuhu@1982";
-$mysql_database = "codewale_details";
-$bd = mysql_connect($mysql_hostname, $mysql_user, $mysql_password) or die("Opps some thing went wrong");
-mysql_select_db($mysql_database, $bd) or die("Opps some thing went wrong");
-
-?>
